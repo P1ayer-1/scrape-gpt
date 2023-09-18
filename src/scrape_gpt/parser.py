f997508361d14bf220d5b55c7d47c9a8ff9d8252
“[Noah <[noahpersaud@zohomail.com]>
2023-09-18 16:20:49 -0400

    Refactor parser and add retrieval model

---
+++ src/scrape_gpt/parser.py
@@ -0,0 +1,166 @@
+import requests
+from selectolax.parser import HTMLParser
+
+header_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+
+class SelectolaxParser():
+    def __init__(self, html):
+        self.tree = HTMLParser(html)
+        self.remove_unwanted_nodes()
+
+        
+    
+
+    
+    def remove_unwanted_nodes(self):
+        tags = ["script", "style"]
+        self.tree.strip_tags(tags, recursive=True)
+
+        nodes_to_remove = []
+        for node in self.tree.root.traverse(include_text=True):
+            tag = node.tag
+            if tag == '-text' and not node.text_content.strip():
+                nodes_to_remove.append(node)
+
+            if tag == '_comment':
+                nodes_to_remove.append(node)
+        
+        for node in nodes_to_remove:
+            node.decompose(recursive=False)
+    
+
+    def count_all_nodes(self):
+        node_count = 0
+        text_count = 0
+        for node in self.tree.root.traverse(include_text=True):
+            node_count += 1
+            if node.tag == '-text':
+                text_count += 1
+        return node_count, text_count
+    
+
+    def traverse_partial_tree(self, start_node, end_node = None, include_text=True, include_self=True):
+        if include_self:
+            yield start_node
+        
+        for node in start_node.traverse(include_text=include_text):
+            if end_node and node == end_node:
+                break
+            yield node
+
+    def traverse_partial_child_tree(self, start_node, end_node = None, include_text=True, include_self=True):
+        if include_self:
+            yield start_node
+        
+        for node in start_node.traverse_children(include_text=include_text):
+            if end_node and node == end_node:
+                break
+            yield node
+
+
+    def traverse_text_nodes(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
+        for node in self.traverse_partial_tree(start_node, end_node=end_node, include_text=True, include_self=include_self):
+            if node.tag == '-text':
+                if node in ignore_nodes:
+                    continue
+                yield node
+
+    def traverse_text_child_nodes(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
+        for node in self.traverse_partial_child_tree(start_node, end_node=end_node, include_text=True, include_self=include_self):
+            if node.tag == '-text':
+                if node in ignore_nodes:
+                    continue
+                yield node
+
+
+    def count_child_nodes(self, start_node):
+        node_count = 0
+        text_count = 0
+        for node in start_node.traverse_children(include_text=True):
+            node_count += 1
+            if node.tag == '-text':
+                text_count += 1
+        return node_count, text_count
+    
+
+    def get_headings(self, start_node):
+        heading_nodes = []
+        heading_text = []
+        for node in start_node.traverse_children(include_text=True):
+            if node.tag in header_tags:
+                text = node.text().strip()
+                heading_text.append(text)
+                heading_nodes.append(node)
+        return heading_nodes, heading_text
+    
+
+    def get_links(self, start_node, end_node = None, include_self=True, ignore_fragments=True):
+        link_nodes = []
+        links = []
+        for node in self.traverse_partial_tree(start_node, end_node=end_node, include_text=False, include_self=include_self):
+            if node.tag == "a":
+                link = node.attributes["href"]
+                if ignore_fragments and link.startswith('#'):
+                    continue
+                links.append(link)
+                link_nodes.append(node)
+
+                
+        return links
+    
+    def get_media_paths(self, node, path="", return_text=True, return_images=True):
+        texts = []
+        images = []
+        for child in node.iter(include_text=True):
+            tag = child.tag
+            new_path = f"{path}.{tag}" if child.tag else path
+            if child.text_content:
+                texts.append((new_path, len(child.text_content.strip())))
+            if tag == "img":
+                images.append((new_path, child.attributes["src"]))
+            if tag == "svg":
+                path_tags = child.css("path")
+                svg_data = []
+                for path_tag in path_tags:
+                    svg_data.append(path_tag.attributes["d"])
+                
+                images.append((new_path, svg_data))
+
+            new_results, imgs = self.get_media_paths(child, new_path, return_text, return_images)
+            if new_results and return_text:
+                texts.extend(new_results)
+            if imgs and return_images:
+                images.extend(imgs)
+        return texts, images
+    
+
+        
+    def get_text_nodes(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
+        # might be better to have a get_x_nodes func instead of separate funcs for each type of node
+        return [node for node in self.traverse_text_nodes(start_node, end_node=end_node, include_self=include_self, ignore_nodes=ignore_nodes)]
+    
+    def get_text_child_nodes(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
+        return [node for node in self.traverse_text_child_nodes(start_node, end_node=end_node, include_self=include_self, ignore_nodes=ignore_nodes)]
+    
+
+    def get_text_lens(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
+        text_lens = []
+        for node in self.traverse_text_nodes(start_node, end_node=end_node, include_self=include_self, ignore_nodes=ignore_nodes):
+            text_lens.append(len(node.text().strip()))
+        return text_lens
+    
+
+    def handle_text_len(self, texts, max_len = 75, trancute=False): # should be in tokens eventually
+        new_texts = []
+        for text in texts:
+            text_len = len(text)
+
+            if text_len > max_len:
+                if trancute:
+                    text = text[:max_len]
+                else:
+                    text = text_len
+            
+
+            new_texts.append(text)
+        return new_texts
--- src/scrape_gpt/scraper.py
+++ src/scrape_gpt/scraper.py
@@ -1,148 +1,67 @@
 import requests
-from selectolax.parser import HTMLParser
+from scrape_gpt.parser import SelectolaxParser
+from typing import List, Dict, Tuple, Union, Optional
+from transformers import BertTokenizerFast, BertModel
+import torch
+from scrape_gpt.types import DeviceMap
 
-header_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 class LlmScraper():
-    def __init__(self, url):
+    
+    def __init__(self, 
+                 url: str, 
+                 parser: str="selectolax", 
+                 load_retrieval_model: bool=True, 
+                 model_name: str="BAAI/bge-large-en-v1.5",
+                 hf_cache_dir: Optional[str]=None,
+                 device_map: Optional[Union[int, str, torch.device, Dict[str, Union[int, str, torch.device]]]]=None,
+                 model: Optional[BertModel]=None,
+                 tokenizer: Optional[BertTokenizerFast]=None,
+                 ):
         self.url = url
         self.html = self.fetch_html(self.url)
-        self.tree = HTMLParser(self.html)
-        self.remove_unwanted_nodes()
+        self.parser = self.get_parser(parser)
+        self.model = model
+        self.tokenizer = tokenizer
+        self.hf_cache_dir = hf_cache_dir
 
+        if device_map and (isinstance(device_map, int) or isinstance(device_map, torch.device)):
+            device_map = {"": device_map}
+
+        if load_retrieval_model:
+            self.init_retrieval_model(model_name, 
+                                      device_map,
+                                      init_tokenizer=self.tokenizer is None, 
+                                      init_model=self.model is None)
+        elif model is not None:
+            self.device = model.device
+        else:
+            self.device = None
         
-    
-    def fetch_html(self, url):
+                
+                
+
+
+    def fetch_html(self, url: str) -> str:
         return requests.get(url).text
     
-    def remove_unwanted_nodes(self):
-        tags = ["script", "style"]
-        self.parser.strip_tags(tags, recursive=True)
 
-        nodes_to_remove = []
-        for node in self.tree.root.traverse(include_text=True):
-            tag = node.tag
-            if tag == '-text' and not node.text_content.strip():
-                nodes_to_remove.append(node)
-
-            if tag == '_comment':
-                nodes_to_remove.append(node)
+    def init_retrieval_model(self, 
+                             model_name: str, 
+                             device_map: Optional[Union[int, str, torch.device, Dict[str, Union[int, str, torch.device]]]]=None, 
+                             hf_cache_dir: Optional[str]=None,
+                             init_tokenizer: bool=True, 
+                             init_model: bool=True) -> None:
+        if init_tokenizer:
+            self.tokenizer = BertTokenizerFast.from_pretrained(model_name, cache_dir=hf_cache_dir)
+        if init_model:
+            self.model = BertModel.from_pretrained(model_name, cache_dir=hf_cache_dir, device_map=device_map)
+        self.device = self.model.device
         
-        for node in nodes_to_remove:
-            node.decompose(recursive=False)
-
-    
-    def get_media_paths(self, node, path="", return_text=True, return_images=True):
-        texts = []
-        images = []
-        for child in node.iter(include_text=True):
-            tag = child.tag
-            new_path = f"{path}.{tag}" if child.tag else path
-            if child.text_content:
-                texts.append((new_path, len(child.text_content.strip())))
-            if tag == "img":
-                images.append((new_path, child.attributes["src"]))
-            if tag == "svg":
-                path_tags = child.css("path")
-                svg_data = []
-                for path_tag in path_tags:
-                    svg_data.append(path_tag.attributes["d"])
-                
-                images.append((new_path, svg_data))
-
-            new_results, imgs = self.get_media_paths(child, new_path, return_text, return_images)
-            if new_results and return_text:
-                texts.extend(new_results)
-            if imgs and return_images:
-                images.extend(imgs)
-        return texts, images
     
 
-    def count_all_nodes(self):
-        node_count = 0
-        text_count = 0
-        for node in self.tree.root.traverse(include_text=True):
-            node_count += 1
-            if node.tag == '-text':
-                text_count += 1
-        return node_count, text_count
-    
-
-
-    def traverse_partial_tree(self, start_node, end_node = None, include_text=True, include_self=True):
-        if include_self:
-            yield start_node
-        
-        for node in start_node.traverse(include_text=include_text):
-            if end_node and node == end_node:
-                break
-            yield node
-
-
-    def count_child_nodes(self, start_node):
-        node_count = 0
-        text_count = 0
-        for node in start_node.traverse_children(include_text=True):
-            node_count += 1
-            if node.tag == '-text':
-                text_count += 1
-        return node_count, text_count
-    
-
-    def get_headings(self, start_node):
-        heading_nodes = []
-        heading_text = []
-        for node in start_node.traverse_children(include_text=True):
-            if node.tag in header_tags:
-                text = node.text().strip()
-                heading_text.append(text)
-                heading_nodes.append(node)
-        return heading_nodes, heading_text
-    
-
-    def get_links(self, start_node, end_node = None, include_self=True, ignore_fragments=True):
-        link_nodes = []
-        links = []
-        for node in self.traverse_partial_tree(start_node, end_node=end_node, include_text=False, include_self=include_self):
-            if node.tag == "a":
-                link = node.attributes["href"]
-                if ignore_fragments and link.startswith('#'):
-                    continue
-                links.append(link)
-                link_nodes.append(node)
-
-                
-        return links
-    
-    def traverse_text_nodes(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
-        for node in self.traverse_partial_tree(start_node, end_node=end_node, include_text=True, include_self=include_self):
-            if node.tag == '-text':
-                if node in ignore_nodes:
-                    continue
-                yield node
-        
-    def get_text_nodes(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
-        return [node for node in self.traverse_text_nodes(start_node, end_node=end_node, include_self=include_self, ignore_nodes=ignore_nodes)]
-    
-
-    def get_text_lens(self, start_node, end_node = None, include_self=True, ignore_nodes=[]):
-        text_lens = []
-        for node in self.traverse_text_nodes(start_node, end_node=end_node, include_self=include_self, ignore_nodes=ignore_nodes):
-            text_lens.append(len(node.text().strip()))
-        return text_lens
-    
-
-    def handle_text_len(self, texts, max_len = 75, trancute=False): # should be in tokens eventually
-        new_texts = []
-        for text in texts:
-            text_len = len(text)
-
-            if text_len > max_len:
-                if trancute:
-                    text = text[:max_len]
-                else:
-                    text = text_len
-            
-
-            new_texts.append(text)
-        return new_texts
+    def get_parser(self, parser: str = "selectolax"):
+        if parser == "selectolax":
+            return SelectolaxParser(self.url)
+        else:
+            raise NotImplementedError(f"Parser {parser} not implemented yet.")
